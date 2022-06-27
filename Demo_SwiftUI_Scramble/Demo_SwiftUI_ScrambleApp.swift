53dac6d36d1cf351cfe3eef519362d19bcf8c214
Tak Liu <tliu@connected.io>
2022-06-27 10:18:22 -0400

    first commit

---
+++ Demo_SwiftUI_Scramble/Demo_SwiftUI_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUI_ScrambleApp.swift
+//  Demo_SwiftUI_Scramble
+//
+//  Created by Tak Liu on 2022-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUI_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
