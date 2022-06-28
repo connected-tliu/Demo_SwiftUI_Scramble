7d9d152d995d092815be0b4bccefb976a703a59f
Tak Liu <tliu@connected.io>
2022-06-28 16:26:36 -0400

    Add error message Add score

--- Demo_SwiftUI_Scramble/ContentView.swift
+++ Demo_SwiftUI_Scramble/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var score = 0
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -20,13 +26,30 @@ struct ContentView: View {
                     .padding()
                     .textInputAutocapitalization(.none)
                 List(usedWords, id: \.self) { usedWord in
-                    Image(systemName: "\(usedWord.count).circle")
-                    Text(usedWord)
+                    HStack {
+                        Image(systemName: "\(usedWord.count).circle")
+                        Text(usedWord)
+                    }
                 }
+                Spacer()
+                Text("Your score: \(score)")
+                    .fontWeight(.heavy)
+                    .font(.largeTitle)
             }
             .navigationTitle(rootWord)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Restart") {
+                        startGame()
+                        resetGameBoard()
+                    }
+                }
+            }
         }
         .onAppear(perform: startGame)
+        .alert(isPresented: $showingError) {
+            Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+        }
     }
     
     func addNewWord() {
@@ -34,8 +57,29 @@ struct ContentView: View {
         
         guard answer.count > 0 else { return }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", messsage: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recongnized", messsage: "You can't just make them up, you know!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", messsage: "that isn't a real word")
+            return
+        }
+        
         usedWords.insert(answer, at: 0)
+        score = gainScore(with: answer)
         newWord = ""
+    }
+    
+    func resetGameBoard() {
+        score = 0
+        usedWords = []
     }
     
     func startGame() {
@@ -48,6 +92,48 @@ struct ContentView: View {
             }
         }
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        guard word.count >= 3 else { return false }
+        guard word != rootWord else { return false }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+       
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, messsage: String) {
+        errorTitle = title
+        errorMessage = messsage
+        showingError = true
+    }
+    
+    func gainScore(with word: String) -> Int {
+        let wordPoint = 1
+        let letterPoint = word.count
+        
+        return wordPoint + letterPoint + score
     }
 }
 
